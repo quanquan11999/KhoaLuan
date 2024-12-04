bf89ff2c85ba0003ae13fdc6a802c4dadb2b1207
vinhtq@tahospital.vn <vinhtq@tahospital.vn>
2024-12-04 16:05:01 +0700

    thêm bộ lọc cho tìm kiếm

--- KhoaLuan/KhoaLuan/ViewModels/PostViewModel.cs
+++ KhoaLuan/KhoaLuan/ViewModels/PostViewModel.cs
@@ -7,52 +7,59 @@ using System.Web;
 
 namespace KhoaLuan.ViewModels
 {
-	public class PostViewModel
-	{
-		public SearchResult Post { get; set; }
-		public List<string> ImageList { get; set; }
-		
+    public class PostViewModel
+    {
+        public SearchResult Post { get; set; }
+        public List<string> ImageList { get; set; }
 
-		public static List<PostViewModel> CreateListPostViewModel(List<SearchResult> listPosts)
-		{
-			List<PostViewModel> list = new List<PostViewModel>();
-			using (var db = new QLTroEntities())
-			{
-				var searchResult = SearchResult.CreateListSearchResult().Where(p=> p.AccountStatus == 1 && p.PostStatus == true).ToList();
-				if (searchResult != null && searchResult.Count > 0)
-				{
-					foreach (var item in searchResult)
-					{
-						list.Add(new PostViewModel()
-						{
-							Post = item,
-							ImageList = GetImage.getListImage(item.MotelID)
-						});
-					}
-				}
-			}
-			return list;
-		}
 
-		public static List<PostViewModel> CreateListPostViewModel(int provinceID,int districtID)
-		{
-			List<PostViewModel> list = new List<PostViewModel>();
-			using (var db = new QLTroEntities())
-			{
-				var searchResult = SearchResult.CreateListSearchResult().Where(p=>p.ProvinceID == provinceID && p.DistrictID == districtID && p.AccountStatus == 1 && p.PostStatus == true).ToList();
-				if (searchResult != null && searchResult.Count > 0)
-				{
-					foreach (var item in searchResult)
-					{
-						list.Add(new PostViewModel()
-						{
-							Post = item,
-							ImageList = GetImage.getListImage(item.MotelID)
-						});
-					}
-				}
-			}
-			return list;
-		}
-	}
+        public static List<PostViewModel> CreateListPostViewModel(List<SearchResult> listPosts)
+        {
+            List<PostViewModel> list = new List<PostViewModel>();
+            using (var db = new QLTroEntities())
+            {
+                var searchResult = SearchResult.CreateListSearchResult().Where(p => p.AccountStatus == 1 && p.PostStatus == true).ToList();
+                if (searchResult != null && searchResult.Count > 0)
+                {
+                    foreach (var item in searchResult)
+                    {
+                        list.Add(new PostViewModel()
+                        {
+                            Post = item,
+                            ImageList = GetImage.getListImage(item.MotelID)
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        public static List<PostViewModel> CreateListPostViewModel(int provinceID, int districtID, int maxPeople)
+        {
+            List<PostViewModel> list = new List<PostViewModel>();
+            using (var db = new QLTroEntities())
+            {
+                var searchResult = SearchResult.CreateListSearchResult()
+                    .Where(p => p.ProvinceID == provinceID && p.DistrictID == districtID
+                    && p.AccountStatus == 1 && p.PostStatus == true)
+                    .ToList();
+                if (maxPeople > 0)
+                {
+                    searchResult = searchResult.Where(x => x.MaxPeople <= maxPeople).ToList();
+                }
+                if (searchResult != null && searchResult.Count > 0)
+                {
+                    foreach (var item in searchResult)
+                    {
+                        list.Add(new PostViewModel()
+                        {
+                            Post = item,
+                            ImageList = GetImage.getListImage(item.MotelID)
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
 }
